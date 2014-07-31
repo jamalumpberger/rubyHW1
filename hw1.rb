8890315cc654bf32369db2a207c4534c07f06fca
jamalumpberger <nozaki.kn@gmail.com>
2014-07-31 14:52:33 -0400

    hw1

---
+++ hw1.rb
@@ -0,0 +1,45 @@
+# 1.  Create the following functions:
+# a. Create a function that takes a string and adds the phrase “Only in America!” to the end of it
+
+def america(product)
+	puts product + " made in America!"
+end
+
+america("jesus:")
+
+# b. Create a function to find the maximum value in an array of numbers. For instance: [100,10,-1000]
+# should return 100.
+
+array = [1, 100, 5, 20, 10, 15, 500]
+x = array.reverse
+puts x[0]
+
+# c. Create a function that takes two arguments - both of them arrays. 
+# Inside of the function, combine the arrays using the items from the first array as keys and the second array as values. 
+# For example, when these two arrays are supplied as arguments:
+# [:toyota, :tesla] [“Prius”, “Model S”]
+# they should return a hash like so: {toyota: “Prius”, tesla: “Model S”}
+
+def combine(a1, a2)
+	hash = Hash[a1.zip a2]
+	puts hash
+end
+
+combine(["yo", "what", "derp"], [10, 1000, 100])
+
+
+# 2. Write a program that prints the numbers from 1 to 100. 
+# But for multiples of three print “Fizz” instead of the number and for multiples of five print “Buzz”. 
+# Print “FizzBuzz” for numbers that are multiples of both 3 and 5.
+
+for x in 1..100 do
+	if ((x % 3 == 0)&&(x % 5 == 0))
+		puts "FizzBuzz"
+	elsif x % 3 == 0
+		puts "Fizz"
+	elsif x % 5 == 0
+		puts "Buzz"
+	else
+		puts x
+	end
+end
